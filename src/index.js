4cbaf0e551c52bb0089992e10570b55f07d52058
DC75166 <daipayanchakraborty75@gmail.com>
2025-04-29 08:30:33 +0530

    Added css styles and imported in index.js file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,1 @@
-const contentBody = document.querySelector('#content');
-const paragraph = document.createElement('div');
-paragraph.textContent = "This is a demo page for a restaurant";
-contentBody.textContent = "This is a test file";
-contentBody.appendChild(paragraph);
-
-const home = document.querySelector(".home");
-const mome = document.querySelector(".menu");
-const about = document.querySelector(".about");
+import './style.css';
